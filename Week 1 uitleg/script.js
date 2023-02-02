51f410e012193f4b51aab09d84c936e3e49d8e8e
Vince van Apeldoorn <vincevanapeldoorn@gmail.com>
2023-02-02 09:40:05 +0100

    Opdracht1.1

---
+++ Week 1 uitleg/script.js
@@ -0,0 +1,11 @@
+/*function myFunction() {
+    document.getElementById("change").innerText = "YOU CLICKED ME!";
+}
+*/
+
+
+let text = document.getElementById("change")
+
+text.style .color ="red";
+text.innerText = "<p>"
+text.fontWeight = "bold"
