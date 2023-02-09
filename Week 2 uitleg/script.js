c81daf8d3dc193f595e461ed8f2a0cd06d6aa82a
Vince van Apeldoorn <vincevanapeldoorn@gmail.com>
2023-02-09 13:37:37 +0100

    opdracht

---
+++ Week 2 uitleg/script.js
@@ -0,0 +1,25 @@
+// let title = document.getElementById("title")
+// let image = document.getElementById("image")
+// let button = document.getElementById("button")
+
+
+
+// function removeImage(){
+//     title.style.color = "blue";
+//     image.style.display = "none";
+
+// }
+// function showImage(){
+//     title.style.color = "black";
+
+//     image.style.display = "block";
+
+// }
+
+
+function calc(num,num2){
+    let answer = num - num2
+    console.log(answer)
+}
+
+calc(7 , 8);
