77dd5e53669096e0cc8be867e08b5674599464e3
Vince1510 <vincevanapeldoorn@gmail.com>
2023-02-16 12:26:11 +0100

    Opdracht 3.1

---
+++ Opdracht 3.1/script.js
@@ -0,0 +1,16 @@
+
+let text = document.getElementById("text")
+let budget = 50;
+let product = 60;
+
+
+if (budget > product){
+    text.innerText ="You are good to go"
+    text.style.color = "green"
+}
+
+else {
+    text.innerText= "You don't have enough money";
+    text.style.color = "red"
+}
+
