0a955e7dbdbd6d162becc8ba3d8b90dea5c93413
Vince1510 <vincevanapeldoorn@gmail.com>
2023-04-09 12:47:17 +0200

    sidebar slider

--- Opdracht 5.2/script.js
+++ Opdracht 5.2/script.js
@@ -1,9 +1,7 @@
-window.onscroll = function() {scrollFunction()};
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-50px";
-  }
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
 }
