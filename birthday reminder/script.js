a56dbba2fbbb8c04a5e3ab83328dbeb60fbd625c
Vince1510 <vincevanapeldoorn@gmail.com>
2023-05-24 11:28:31 +0200

    birthday

---
+++ birthday reminder/script.js
@@ -0,0 +1,35 @@
+//id ophalen vanuit de html
+document.addEventListener('DOMContentLoaded', function () {
+  const container = document.getElementById('container');
+  const countElement = document.getElementById('count');
+  const clearBtn = document.getElementById('clearBtn');
+  let data = [];
+
+  //data vanuit de json op het scherm laten zien
+  function displayData() {
+    container.innerHTML = '';
+    countElement.textContent = data.length;
+
+    data.forEach(person => {
+      const personElement = document.createElement('div');
+      personElement.classList.add('person');
+      personElement.innerHTML = `
+        <h3>${person.name}</h3>
+        <p>Age: ${person.age}</p>
+        <img src="${person.image}">
+      `;
+      container.appendChild(personElement);
+    });
+  }
+
+  //eventlistener voor de clear button
+  clearBtn.addEventListener('click', () => container.innerHTML = '');
+
+  //Data ophalen vanuit de jason
+  fetch('data.json')
+    .then(response => response.json())
+    .then(jsonData => {
+      data = jsonData;
+      displayData();
+    });
+});
